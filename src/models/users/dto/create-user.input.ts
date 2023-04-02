f7d6418fb61ffc8da804c1aa814d1a06ae6de1eb
Carlos <carlosaugusto@bodyfitteam.com.br>
2023-04-02 02:22:24 -0300

    Criado a validação do email repetido e verificação de campos nao existentes

--- src/models/users/dto/create-user.input.ts
+++ src/models/users/dto/create-user.input.ts
@@ -1,16 +1,20 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => String)
-  id: string;
+  @Field(() => String, {nullable: true})
+  id?: string;
 
+  @IsEmail({}, { message: 'Email inválido' })
   @Field(() => String)
   email: string;
 
+  @IsNotEmpty({ message: 'A senha é obrigatório' })
   @Field(() => String)
   password: string;
 
+  @IsNotEmpty({ message: 'O nome é obrigatória' })
   @Field(() => String)
   name: string;
 }
--- src/models/users/repositories/UserRepository.ts
+++ src/models/users/repositories/UserRepository.ts
@@ -1,6 +1,6 @@
 import { User } from '@prisma/client';
 import { IUserRepository } from './IUserRepository';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../services/prima.service';
 import { CreateUserInput } from '../dto/create-user.input';
 
@@ -10,7 +10,19 @@ class UserRepository implements IUserRepository {
   constructor(private prisma: PrismaService) {};
 
   async create(data: CreateUserInput): Promise<User> {
-    const user = await this.prisma.user.create({ data });
+    const isUserRepeted = await this.prisma.user.findUnique({
+      where: {
+        email: data.email
+      }
+    });
+    if(!!isUserRepeted){
+      throw new HttpException(
+        'Esse email já é usado por outro usuário. Por favor, tente outro.',
+         HttpStatus.BAD_REQUEST
+      );
+    }
+
+    const user: User = await this.prisma.user.create({ data });
     return user;
   }
   getAll(): Promise<User[]> {
