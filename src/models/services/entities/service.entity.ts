faaf8dfe59efedefbfbe1274ad1869b21303aab9
iCarlosAugusto <augusto.dev@proton.me>
2023-05-08 00:02:55 -0300

    Arrumando o service entity

--- src/models/services/entities/service.entity.ts
+++ src/models/services/entities/service.entity.ts
@@ -2,6 +2,19 @@ import { ObjectType, Field } from '@nestjs/graphql';
 
 @ObjectType()
 export class Service {
+
+  @Field(() => String)
+  id: string;
+
   @Field(() => String)
   name: string;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => String)
+  price: string;
+
+  @Field(() => String)
+  partnerId: string;
 }
