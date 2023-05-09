71ed3e5210e75e2cfc4cbc859d2a5e6201bb6cb4
iCarlosAugusto <augusto.dev@proton.me>
2023-05-09 17:28:36 -0300

    Adicionado o partner no serviceEntity

--- src/models/services/entities/service.entity.ts
+++ src/models/services/entities/service.entity.ts
@@ -1,13 +1,17 @@
 import { ObjectType, Field } from '@nestjs/graphql';
+import { PartnerEntity } from 'src/models/partners/entities/partner.entity';
 
 @ObjectType()
-export class Service {
+export class ServiceEntity {
 
   @Field(() => String)
   id: string;
 
   @Field(() => String)
   name: string;
+
+  @Field(() => PartnerEntity)
+  partner: PartnerEntity;
 
   @Field(() => String)
   description: string;
--- src/models/services/services.resolver.ts
+++ src/models/services/services.resolver.ts
@@ -1,35 +1,35 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { ServicesService } from './services.service';
-import { Service } from './entities/service.entity';
+import { ServiceEntity } from './entities/service.entity';
 import { CreateServiceInput } from './dto/create-service.input';
 import { UpdateServiceInput } from './dto/update-service.input';
 import { FindAllServicesInput } from './dto/find-all-services.input';
 
-@Resolver(() => Service)
+@Resolver(() => ServiceEntity)
 export class ServicesResolver {
   constructor(private readonly servicesService: ServicesService) {}
 
-  @Mutation(() => Service)
+  @Mutation(() => ServiceEntity)
   createService(@Args('createServiceInput') createServiceInput: CreateServiceInput) {
     return this.servicesService.create(createServiceInput);
   }
 
-  @Query(() => [Service], {nullable: true})
+  @Query(() => [ServiceEntity], {nullable: true})
   findAllServices(@Args('findAllPartnersInput') findAllServicesInput: FindAllServicesInput) {
     return this.servicesService.findAll(findAllServicesInput);
   }
 
-  @Query(() => Service, { name: 'service' })
+  @Query(() => ServiceEntity, { name: 'service' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.servicesService.findOne(id);
   }
 
-  @Mutation(() => Service)
+  @Mutation(() => ServiceEntity)
   updateService(@Args('updateServiceInput') updateServiceInput: UpdateServiceInput) {
     return this.servicesService.update(updateServiceInput.id, updateServiceInput);
   }
 
-  @Mutation(() => Service)
+  @Mutation(() => ServiceEntity)
   removeService(@Args('id', { type: () => Int }) id: number) {
     return this.servicesService.remove(id);
   }
