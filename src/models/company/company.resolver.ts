43e1777c7b52e163744007f745b37f43939a346d
iCarlosAugusto <augusto.dev@proton.me>
2023-05-29 21:51:18 -0300

    Criado o serviço para pesquisa a company pela data

--- src/models/company/company.resolver.ts
+++ src/models/company/company.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { CompanyEntity } from './entities/company.entity';
 import { CompanyService } from './company.service';
 import { CreateCompanyInput } from './dtos/create-company.input';
+import { FindCompaniesByDateInput } from './dtos/find-companies-by-date.input';
 
 @Resolver(() => CompanyEntity)
 export class CompanyResolver {
@@ -17,5 +18,12 @@ export class CompanyResolver {
   @Query(() => [CompanyEntity])
   findAllCompanies() {
     return this.companyService.findAll();
+  }
+
+  @Query(() => [CompanyEntity])
+  findCompaniesByDate(
+    @Args('findCompaniesByDate') findCompaniesByDate: FindCompaniesByDateInput,
+  ) {
+    return this.companyService.findByDate(findCompaniesByDate);
   }
 }
--- src/models/company/company.service.ts
+++ src/models/company/company.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyInput } from './dtos/create-company.input';
 import { PrismaService } from '../users/services/prima.service';
+import { FindCompaniesByDateInput } from './dtos/find-companies-by-date.input';
 
 @Injectable()
 export class CompanyService {
@@ -17,5 +18,13 @@ export class CompanyService {
 
   async findAll() {
     return await this.prisma.company.findMany();
+  }
+
+  async findByDate({ date }: FindCompaniesByDateInput) {
+    return this.prisma.company.findMany({
+      where: {
+        availableDay: date
+      }
+    })
   }
 }
